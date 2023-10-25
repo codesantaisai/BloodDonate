23200d70fb9c4ef2af8713ec666d4bc31e987d69
harrypotter <jathurshan@protonmail.com>
2023-10-25 12:03:07 +0530

    secondCommit

--- BloodDonate/Program.cs
+++ BloodDonate/Program.cs
@@ -1,11 +1,18 @@
+using BloodDonate.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<EmployeeDbContext>(Options =>
+    Options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -15,6 +22,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors(builder=>
+    builder.
+        AllowAnyOrigin().
+        AllowAnyMethod().
+        AllowAnyHeader()
+    );
 
 app.UseHttpsRedirection();
 
